7a41fe26774f996b5ab8a55face6fbc29c66fdbb
KevinW <bird831205@hotmail.com>
2020-01-19 16:47:38 -0500

    form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoteComponent } from './vote/vote.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoteComponent
+    VoteComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent {
+
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+    this.form = fb.group({
+      name: ['', Validators.required],
+      email: ['']
+    })
+  }
+
+
+}
