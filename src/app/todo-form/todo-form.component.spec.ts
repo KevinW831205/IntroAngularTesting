9b06556781235853763f5a8fe8b62d31b6beba9a
KevinW <bird831205@hotmail.com>
2020-01-19 16:52:17 -0500

    form testing"

--- src/app/todo-form/todo-form.component.spec.ts
+++ src/app/todo-form/todo-form.component.spec.ts
@@ -10,5 +10,21 @@ describe('TodoFormComponent', () => {
     component = new TodoFormComponent(new FormBuilder);
   })
 
+  it('should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  })
+
+  it('should make the name control required', () => {
+    let control = component.form.get('name');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
+
+    control.setValue('enteredsomething');
+    expect(control.valid).toBeTruthy();
+
+    
+  })
+
 
 });
