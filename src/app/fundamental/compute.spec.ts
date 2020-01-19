1363ffc14af36a299d5c6abd5986b767ef8c7372
KevinW <bird831205@hotmail.com>
2020-01-18 21:44:32 -0500

    abstracting the testing

--- src/app/fundamental/compute.spec.ts
+++ src/app/fundamental/compute.spec.ts
@@ -1,33 +1,44 @@
 import { compute } from "./compute";
 
+function testCompute(expected, input){
+    const actual = compute(input);
+    expect(actual).toBe(expected);
+}
+
 describe('compute', () => { // suite definition
     // spec
     it('return 0 if input is negative', () => {
         // given
-        const expected = 0;
-        const input = -1
+        // const expected = 0;
+        // const input = -1
 
-        //when
-        const actual = compute(input);
+        // //when
+        // const actual = compute(input);
 
-        //then
-        expect(actual).toBe(expected);
+        // //then
+        // expect(actual).toBe(expected);
+
+        testCompute(0, -2)
     })
+
 
     it('return the number plus 1 if input is greater than or equal to 0', () => {
-        // given
-        const expected1 = 1;
-        const expected2 = 2;
-        const input1 = 0;
-        const input2 = 1;
+        testCompute(1,0);
+        testCompute(2,1)
         
-        // given 
-        const actual1 = compute(input1);
-        const actual2 = compute(input2);
+        // // given
+        // const expected1 = 1;
+        // const expected2 = 2;
+        // const input1 = 0;
+        // const input2 = 1;
+        
+        // // when
+        // const actual1 = compute(input1);
+        // const actual2 = compute(input2);
 
-        //then
-        expect(expected1).toBe(actual1);
-        expect(expected2).toBe(actual2);
+        // //then
+        // expect(expected1).toBe(actual1);
+        // expect(expected2).toBe(actual2);
     })
 
-})  
+})
