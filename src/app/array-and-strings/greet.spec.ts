4d5a3f8edc4a8f70b45b9984b9121fefd530e91f
KevinW <bird831205@hotmail.com>
2020-01-18 21:54:36 -0500

    expect().toContain() and testing string in general

---
+++ src/app/array-and-strings/greet.spec.ts
@@ -0,0 +1,22 @@
+import { greet } from "./greet";
+
+function testGreetContain(input){
+    const actual = greet(input)    
+    expect(actual).toContain(input);
+}
+
+function testGreetDetail(expected ,input){
+    const actual = greet(input);
+    expect(actual).toBe(expected);
+}
+
+describe('greet', () => {
+    it('should add "Welcome" before the input', ()=>{
+        testGreetDetail("Welcome abc","abc");
+    });
+
+    it('should contain the input', ()=>{
+        testGreetContain("abcdefg")
+    });
+
+})
