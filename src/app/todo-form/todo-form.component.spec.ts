33816c0afa605552db512b511653fa80f9fa7c70
KevinW <bird831205@hotmail.com>
2020-01-19 16:49:13 -0500

    form test setup

--- src/app/todo-form/todo-form.component.spec.ts
+++ src/app/todo-form/todo-form.component.spec.ts
@@ -1,7 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoFormComponent } from './todo-form.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
+
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder);
+  })
+
 
 });
