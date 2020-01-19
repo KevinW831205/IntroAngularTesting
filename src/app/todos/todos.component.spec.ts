2bd6cbbbc11febc0883cd8abcf06c4934b7b709b
KevinW <bird831205@hotmail.com>
2020-01-19 17:54:54 -0500

    working with some observable

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodosComponent } from './todos.component';
+import { TodoserviceService } from '../todoservice.service';
+import { Observable } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
-  let fixture: ComponentFixture<TodosComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
-    })
-    .compileComponents();
-  }));
+  let service: TodoserviceService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TodosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    service = new TodoserviceService(null);
+    component = new TodosComponent(service);
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should set todos property with the items returned', () => {
+    spyOn(service, 'getTodos').and.callFake(() => {
+      const array = ['todo1', 'todo2'];
+
+      return new Observable((obs) => {
+        obs.next(array);
+      })
+    })
+  })
+
+
+
+
 });
