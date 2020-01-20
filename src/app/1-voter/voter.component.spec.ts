e1b0be1dc4491e9bd67f500c19d69dabe11ed5e3
KevinW <bird831205@hotmail.com>
2020-01-20 16:01:26 -0500

    configuring testbed

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,8 +1,14 @@
 import { VoterComponent } from './voter.component';
+import { TestBed } from '@angular/core/testing';
 
 describe('VoterComponent', () => {
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [VoterComponent]
+    });
+
+    TestBed.createComponent(VoterComponent);
   });
 
   it('', () => {
