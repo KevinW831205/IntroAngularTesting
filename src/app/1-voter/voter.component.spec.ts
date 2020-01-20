76e4d7319f240a0e7ec77fd732cc54e19d8715d4
KevinW <bird831205@hotmail.com>
2020-01-20 17:27:25 -0500

    testing event bindings

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -19,23 +19,30 @@ describe('VoterComponent', () => {
   });
 
   it('Should render total votes', () => {
-    component.othersVote =20;
+    component.othersVote = 20;
     component.myVote = 1;
 
     fixture.detectChanges();
 
-    let de =  fixture.debugElement.query(By.css('.vote-count'));
-    let el:HTMLElement =  de.nativeElement;
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
 
     expect(el.innerText).toContain('21');
   });
 
-  it('should high light the upvote button if I have upvoted'),()=>{
+  it('should high light the upvote button if I have upvoted'), () => {
     component.myVote = 1;
     fixture.detectChanges();
 
     let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'))
-    
+
     expect(de.classes['highlighted']).toBeTruthy();
   }
+
+  it('should increase total votes when upvote button is clicked', () => {
+    let button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    button.triggerEventHandler('click',null);
+
+    expect(component.totalVotes).toBe(1);
+  })
 });
