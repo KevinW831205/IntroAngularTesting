cf40a6c37157ed7d73e6a17b95332a1f0cd9e0e8
KevinW <bird831205@hotmail.com>
2020-01-21 10:54:05 -0500

    creating router stubs

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -4,16 +4,33 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
-xdescribe('UserDetailsComponent', () => {
+import { Observable } from 'rxjs';
+
+class RouterStub {
+  navigate(params) {
+
+  }
+}
+
+class ActivatedRouteStub {
+  params: Observable<any> = Observable.create();
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [UserDetailsComponent],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }        
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
